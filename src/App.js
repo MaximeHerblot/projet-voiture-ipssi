fbfc3598c27a28b35767a6eaf5b450a287593938
maximeh <m.herblot@neofly.com>
2023-09-27 17:33:00 +0200

    ajout bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from './core/routes/MainRoutes';
 
 function App() {
