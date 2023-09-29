a229f689beec8172011124bdcb5d87040e5dc554
maximeh <m.herblot@neofly.com>
2023-09-29 09:17:51 +0200

    layout

--- src/core/composants/layout/Layout.js
+++ src/core/composants/layout/Layout.js
@@ -1,47 +1,42 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-//import { useContext } from "react";
-//import LoginContext from "../store/login-context";
+import { useContext } from "react";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import LoginContext from "../../store/login-context";
 
 const Layout = () => {
- // const login = useContext(LoginContext);
+  const login = useContext(LoginContext);
 
   return (
-    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
+    <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link>
-              <NavLink to={"/"}>Page d'acceuil</NavLink>
+              <NavLink to={"/"}>Home</NavLink>
             </Nav.Link>
             <Nav.Link>
               <NavLink to={"/voitures"}>Listes des voitures</NavLink>
             </Nav.Link>
+            {login.logged && (
+              <Nav.Link>
+                <NavLink to={"/voitures/creation"}>
+                  Création d'une voiture
+                </NavLink>
+              </Nav.Link>
+            )}
             <Nav.Link>
-              <NavLink to={"/voitures/creation"}>
-                Création d'une voiture
-              </NavLink>
+              {login.email === "" && (
+                <NavLink to={"/login"}>Se connecter</NavLink>
+              )}
+              {login.email !== "" && (
+                <>
+                  <span>{login.email}</span>
+                  <NavLink to={"/logout"}>Se déconnecter</NavLink>
+                </>
+              )}
             </Nav.Link>
           </Nav>
-
-            {/* Barre de recherche tout à droite */}
-          <div className="col-md-6 ms-auto">
-            <form action="">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Recherche"
-                />
-                <button type="submit" className="btn btn-outline-light">
-                  Rechercher
-                </button>
-              </div>
-            </form>
-          </div>
-          {/* Fin de la barre de recherche */}
-          
         </Navbar.Collapse>
       </Container>
     </Navbar>
