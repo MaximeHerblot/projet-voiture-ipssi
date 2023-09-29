185737479231de7b0a6aaa979c4e756cc4ee10f9
doudouaaaaaa <douaa.abid01@gmail.com>
2023-09-29 11:08:37 +0200

    fond

--- src/core/composants/layout/Layout.js
+++ src/core/composants/layout/Layout.js
@@ -6,28 +6,34 @@ import LoginContext from "../../store/login-context";
 const Layout = () => {
   const login = useContext(LoginContext);
 
+  const linkStyle = {
+    color: "white",        // Couleur du texte en blanc
+    textDecoration: "none", // Supprime la décoration du lien
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+   
+    <Navbar expand="lg" bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link>
-              <NavLink to={"/"}>Home</NavLink>
+              <NavLink to={"/"}style={linkStyle}>  Page d'acceuil</NavLink>
             </Nav.Link>
             <Nav.Link>
-              <NavLink to={"/voitures"}>Listes des voitures</NavLink>
+              <NavLink to={"/voitures"}style={linkStyle}>  Listes des voitures</NavLink>
             </Nav.Link>
             {login.logged && (
               <Nav.Link>
-                <NavLink to={"/voitures/creation"}>
+                <NavLink to={"/voitures/creation"}style={linkStyle}>
                   Création d'une voiture
                 </NavLink>
               </Nav.Link>
             )}
             <Nav.Link>
               {login.email === "" && (
-                <NavLink to={"/login"}>Se connecter</NavLink>
+                <NavLink to={"/login"}style={linkStyle}> Se connecter</NavLink>
               )}
               {login.email !== "" && (
                 <>
